7e64c33142d57d443d9f1e7e033d9a8b475d744f
Olothfael <olothfael@gmail.com>
2026-05-18 10:24:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'essensplan-v1';
-const ASSETS = ['/', '/Essensplan2.html', '/manifest.json', '/icons/icon-192.svg', '/icons/icon-512.svg'];
+const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.svg', './icons/icon-512.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
